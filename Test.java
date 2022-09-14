d8994bd14e0cb7c929b06968786d37f38656b585
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-09-14 22:04:37 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+package com.edu.dee;
+
+public class Test {
+
+	public static void main(String[] args) {
+		byte a,b,c;
+		
+		a=123;
+		b=98;
+		c=(byte)(a+b);
+		System.out.println("c="+c);
+				
+	}
+
+}
