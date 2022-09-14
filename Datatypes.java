dfe4584c857deb27a940b4618ba83bd89984e007
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-09-14 22:03:31 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,28 @@
+package com.edu.dee;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		int i=0;
+		byte b=126;
+		short s=567;
+		long l=876L;
+		float f=567.5f;
+		double d=98798.56;
+		char ch='A';
+		char chr=66;
+		
+		boolean bool=true;
+		
+		System.out.println("integer value="+i); 
+		System.out.println("short value="+s);
+		System.out.println("byte value"+b);
+		System.out.println("long value"+l);
+		System.out.println("float value"+f);
+		System.out.println("double d="+d);
+		System.out.println("char value="+ch);
+		System.out.println("char value using ascii="+chr);
+		System.out.println("boolean value="+bool);
+	}
+
+}
