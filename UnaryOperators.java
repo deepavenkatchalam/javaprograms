c6814477127b0726d6a737dce341f479e343308d
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-09-19 21:45:35 +0530

    Add files via upload

---
+++ UnaryOperators.java
@@ -0,0 +1,17 @@
+package com.edu.dee;
+
+public class UnaryOperators {
+
+	public static void main(String[] args) {
+		int k, j;
+		k=2;
+		j=k++;
+		System.out.println("j="+j);
+		System.out.println("k="+k);
+		
+		
+		
+
+	}
+
+}
