c42a46d6738a6f6fc2c2207d88faff5422f1f188
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-09-19 21:45:00 +0530

    Add files via upload

---
+++ Comparisonop.java
@@ -0,0 +1,21 @@
+package com.edu.dee;
+
+public class Comparisonop {
+
+	public static void main(String[] args) {
+		int i=10,j=30;
+		boolean b=i>j;
+		System.out.println(b);
+		System.out.println("b="+b);
+		
+		b=i<j;
+		System.out.println(b);
+		
+		b=(i==j);
+		System.out.println("both are equal or not"+b);
+	
+
+	}
+
+}
+
