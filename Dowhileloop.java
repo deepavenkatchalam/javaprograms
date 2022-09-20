408e4251f1c84b1d0683ed954f03e05efd300162
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-09-20 15:57:56 +0530

    Add files via upload

---
+++ Dowhileloop.java
@@ -0,0 +1,18 @@
+package com.kode;
+
+public class Dowhileloop {
+
+	public static void main(String[] args) {
+		int i;
+		i=1;
+
+			i=1;
+			do {
+				System.out.println(i);
+				i++;
+		
+		}
+			while(i<=50);
+	}
+
+}
