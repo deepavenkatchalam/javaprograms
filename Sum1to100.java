20d977a68905afb9cf931a554413addabc3dadfc
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-09-20 16:03:51 +0530

    Add files via upload

---
+++ Sum1to100.java
@@ -0,0 +1,16 @@
+package com.kode;
+
+public class Sum1to100 {
+
+	public static void main(String[] args) {
+		int i,s=0;
+		i=1;
+		while(i<=100) {
+			s=s+i;
+			i=i+2;
+					
+		}
+		System.out.println("total of 1 to 100 natural numbers ="+s);
+	}
+
+}
