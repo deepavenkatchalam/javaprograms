0aece7dd29f9666a6a179be5de4990532e98726a
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-09-20 16:00:57 +0530

    Add files via upload

---
+++ MainAppLoop.java
@@ -0,0 +1,14 @@
+package com.kode;
+
+public class MainAppLoop {
+
+	public static void main(String[] args) {
+		int i;
+		i=1;
+		//while(i<=5)
+			//System.out.println("deepa");
+		i=i+1;
+
+	}
+
+}
