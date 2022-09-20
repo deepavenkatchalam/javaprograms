0e4bf44a5f53b7531812bafde9232dd1b6991adc
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-09-20 16:11:57 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,24 @@
+package com.deepu;
+
+public class arithmetic {
+
+	public static void main(String[] args) {
+		int num1, num2, ans;
+		num1=20;
+		num2=15;
+		
+		//subtraction
+		
+		ans=num1-num2;
+		System.out.println("subtraction result="+ans);
+		
+		//multiplication
+		ans=num1*num2;
+		System.out.println("multpication result="+ans);
+		
+		//division
+		ans=num1/num2;
+		System.out.println("division result="+ans);	
+	}
+
+}
