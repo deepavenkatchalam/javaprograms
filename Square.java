08f5543ec68374f4db3a367b5d123f0d627f112a
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-09-20 16:14:47 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,12 @@
+package com.deepu;
+
+public class Square {
+
+	public static void main(String[] args) {
+		int s=13;
+		int area_square=s*s;
+		System.out.println("Area of the square="+area_square);
+		
+	}
+
+}
