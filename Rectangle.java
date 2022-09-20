ea983f071a2108744c62c248fb1ad24c3b83b206
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-09-20 16:14:07 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,13 @@
+package com.deepu;
+
+public class Rectangle {
+
+	public static void main(String[] args) {
+		int width=5;
+		int height=10;
+		int area=width*height;
+		System.out.println("Area of rectangle="+area);
+
+	}
+
+}
