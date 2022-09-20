311acd6a0d35942925ef26e7ef703d8bda046907
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-09-20 15:59:46 +0530

    Add files via upload

---
+++ ForloopPrint1to100.java
@@ -0,0 +1,14 @@
+package com.kode;
+
+public class ForloopPrint1to100 {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=100;i>=1;i=i--) {
+			System.out.println(i);
+		}
+		System.out.println(i);
+
+	}
+
+}
