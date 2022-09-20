baf118919162aa03265f0363200304952b3d69e2
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-09-20 16:03:02 +0530

    Add files via upload

---
+++ PrintallFactors.java
@@ -0,0 +1,13 @@
+package com.kode;
+
+public class PrintallFactors {
+
+	public static void main(String[] args) {
+		int number=6;
+		System.out.println("factors of the number is");
+		for(int i=1;i<=number;i++) {
+			if{number%i==0){
+				System.out.println(i);
+			}
+		}
+
