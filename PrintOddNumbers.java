ffa432528a74746e2bcc6fbbc8716cf89aba8649
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-09-20 16:07:03 +0530

    Add files via upload

---
+++ PrintOddNumbers.java
@@ -0,0 +1,15 @@
+package com.kode;
+
+public class PrintOddNumbers {
+
+	public static void main(String[] args) {
+		int i;
+		i=1;
+		while(i<=100) {
+			System.out.println(i);
+			i=i+2;
+		}
+
+	}
+
+}
