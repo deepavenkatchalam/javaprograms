e1bbcd8ecc3365c925ed75f1b73e0755ad12ac40
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-09-29 13:15:31 +0530

    Add files via upload

---
+++ CompareString.java
@@ -0,0 +1,13 @@
+package com.str;
+
+public class CompareString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="a";
+		String s2="a";
+		System.out.println(s1.compareTo(s2));
+
+	}
+
+}
