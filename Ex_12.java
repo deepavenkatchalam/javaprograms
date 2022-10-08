349d7bfa5208686f946e585ffafd52aba32c71e8
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-10-08 14:25:31 +0530

    Add files via upload

---
+++ Ex_12.java
@@ -0,0 +1,17 @@
+package com.ex;
+
+public class Ex_12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		do {
+			System.out.println("SkillingCSStudent "+i);
+			i++;
+		
+		}
+		while(i<=10);
+
+	}
+
+}
