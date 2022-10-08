fd543b4eb30f93357d1e67b274e5020c3ce45433
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-10-08 14:27:55 +0530

    Add files via upload

---
+++ Ex_16.java
@@ -0,0 +1,17 @@
+package com.ex;
+
+
+public class Ex_16 {
+	Ex_16()
+	{
+		System.out.println("creating a default constructor");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ex_16 ob=new Ex_16();
+		
+
+	}
+
+}
