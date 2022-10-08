7a4f01e84488486dd6858abd3adc7b599548a0bf
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-10-08 14:28:44 +0530

    Add files via upload

---
+++ Ex_17.java
@@ -0,0 +1,21 @@
+package com.ex;
+
+class Argue{
+	int i;
+	Argue(){
+		i=10;
+		System.out.println("creating a no arguement constructor");
+		System.out.println("i="+i);
+	}
+}
+
+public class Ex_17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Argue arg=new Argue();
+		
+
+	}
+
+}
