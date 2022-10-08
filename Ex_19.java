ab78575c8a2d3afb4351842bc58139d4c044380f
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-10-08 14:29:17 +0530

    Add files via upload

---
+++ Ex_19.java
@@ -0,0 +1,21 @@
+package com.ex;
+
+public class Ex_19 {
+	void sum(int a,long b)
+	{
+		System.out.println(a+b);
+	}
+	void sum(int a,int b,int c)
+	{
+		System.out.println(a+b+c);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ex_19 ob=new Ex_19();
+		ob.sum(50, 50);
+		ob.sum(50, 50, 50);
+		
+	}
+
+}
