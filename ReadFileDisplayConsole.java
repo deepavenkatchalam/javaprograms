e910ccad70a96ad931870ce1c33584e980534918
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-10-19 13:14:40 +0530

    Add files via upload

---
+++ ReadFileDisplayConsole.java
@@ -0,0 +1,26 @@
+package com.iooper;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReadFileDisplayConsole {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		FileInputStream fis=new FileInputStream("courses.txt");
+		  BufferedReader br=new BufferedReader(new InputStreamReader(fis));
+	       String ln;
+	       while((ln=br.readLine())!=null) {
+	    	   System.out.println(ln);
+	       }
+	       
+	    	  fis.close();
+	    	  br.close();
+
+
+	}
+
+}
