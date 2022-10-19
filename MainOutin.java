c380691dd97443d5f39d48112fd74d3bd415952a
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-10-19 13:12:05 +0530

    Add files via upload

---
+++ MainOutin.java
@@ -0,0 +1,13 @@
+package com.oper;
+
+import java.io.OutputStream;
+
+public class MainOutin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OutputStream oStream=new fileOutputStream("Output.txt");
+
+	}
+
+}
