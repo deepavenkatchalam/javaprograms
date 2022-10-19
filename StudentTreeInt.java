50f92c1c380c9b9cd0fb206d67e53c5f2fabb920
deepavenkatchalam <113506535+deepavenkatchalam@users.noreply.github.com>
2022-10-19 13:32:36 +0530

    Add files via upload

---
+++ StudentTreeInt.java
@@ -0,0 +1,22 @@
+package com.intma;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class StudentTreeInt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeMap<String,Long>mp=new TreeMap<String,Long>();
+		mp.put("deepu",(long) 9187725967L);
+		mp.put("pri",(long) 9234986547L);
+		mp.put("vino",(long) 9345278998L);
+		mp.put("madhu",(long) 9474268097L);
+		mp.put("sathi",(long) 9542479075L);
+		System.out.println(mp);
+		
+		
+		
+
+	}
+
+}
